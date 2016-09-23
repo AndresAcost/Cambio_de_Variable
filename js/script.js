bd53815895429f309c81122cf974fe09ccee1374
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2016-09-23 00:20:23 -0500

    Primera aproximacion de las raices

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
+function convertirAregloATexto(arreglo) {
+  var texto = '( '
+  for (var i = 0; i < arreglo.length; i++) {
+    texto += arreglo[i] + ", "
+  }
+  texto = texto.slice(0, -2) //quita los dos últimos caracteres
+  texto += ' )'
+}
+
 function calcularFuncion(rutaPlantilla) {
 
+  var A = document.getElementById('A').value
+  var B = document.getElementById('B').value
+  var coeficientesPolinomio = [A, B]
+  var raices = Raices.calcular(coeficientesPolinomio)
+
   var diccionario = {
-    "A": document.getElementById('A').value,
-    "B": document.getElementById('B').value,
+    'A': A,
+    'B': B,
+    'raiz1': raices[0],
+    'raiz2': raices[1],
   }
 
   function listener(htmlParseado) {
     var muestraproceso = document.getElementById('muestraproceso')
-    muestraproceso.innerHTML = ""
+    muestraproceso.innerHTML = ''
     muestraproceso.innerHTML = htmlParseado
   }
 
@@ -28,7 +44,7 @@ function leerPlantilla(diccionario, listener, rutaPlantilla) {
   }
 
   xhr.onerror = function() {
-    console.log("Unable to load RSS")
+    console.log('Unable to load RSS')
   }
 
   // Part 5 - Starting the process
@@ -38,7 +54,7 @@ function leerPlantilla(diccionario, listener, rutaPlantilla) {
 function reemplazarVariables(respuestaHTML, diccionario, listener) {
   var htmlParseado = respuestaHTML
   for (var i in diccionario) {
-    var expresionRegular = new RegExp("{{" + i + "}}", 'g')
+    var expresionRegular = new RegExp('{{' + i + '}}', 'g')
     htmlParseado = htmlParseado.replace(expresionRegular, diccionario[i]);
   }
   listener(htmlParseado)
