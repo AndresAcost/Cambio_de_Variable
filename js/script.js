b28401f43f70dfdfdabfc6c9b0ae7b73658a185b
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2016-09-22 23:53:09 -0500

    Se le agrega plantilla para la segunda

--- js/script.js
+++ js/script.js
@@ -1,56 +1,34 @@
-function calcularFuncion1() {
+function calcularFuncion(rutaPlantilla) {
+
+  var diccionario = {
+    "A": document.getElementById('A').value,
+    "B": document.getElementById('B').value,
+  }
+
+  function listener(htmlParseado) {
     var muestraproceso = document.getElementById('muestraproceso')
-
-  /*  var CELDA = 'F_n=' + a + ' + ' + b + '*f_(n/2)';
-    CELDA += '<br></br> Considerando que: n= 2^i';
-    CELDA += '<br></br> F_2^i = ' + a + ' + ' + b + '* f_2^(i-1) ';
-    b = b * i
-    CELDA += '<br></br> F_2^i ' + b + '* f_2^(i-1) = ' + a;
-    CELDA += '<br></br> Cambiando los subindices en función i, tenemos';
-    CELDA += '<br></br> F_i  ' + b + ' *f_(i-1) = ' + a;
-    CELDA += '<br></br> Reemplazando i por i-1, y multiplicando por (-1) tenemos:';
-    c = a * i
-    d = b * i
-    CELDA += '<br></br>' + i + ' F_(i-1) + ' + d + ' f_(i-2) = ' + c;
-    CELDA += '<br></br> Sumando la ecuaciones se convierte en homogenea:';
-    k1 = (b + i)
-    k2 = 0 //a+c
-    CELDA += '<br></br> F_i ' + k1 + ' f_(i-1) + ' + d + ' f_(i-2) = ' + k2;
-    dios=1
-    coeficientes=new Array[dios,k1,d]
-    var raices = Raices.calcular(coeficientes)
-    console.log(raices);
-    muestraproceso.innerHTML = CELDA*/
-
     muestraproceso.innerHTML = ""
+    muestraproceso.innerHTML = htmlParseado
+  }
 
-    var diccionario = {
-      "A": document.getElementById('A').value,
-      "B": document.getElementById('B').value,
-    }
-
-    function listener(htmlParseado) {
-        muestraproceso.innerHTML = htmlParseado
-    }
-
-    leerPlantilla(diccionario, listener)
+  leerPlantilla(diccionario, listener, rutaPlantilla)
 }
 
-function leerPlantilla(diccionario, listener) {
+function leerPlantilla(diccionario, listener, rutaPlantilla) {
   // Part 3 - Creating and setting up the XHR object
   var xhr = new XMLHttpRequest() //elemento de js no en todos esta
 
-  xhr.open('GET', "template1.html")
+  xhr.open('GET', rutaPlantilla)
 
   // Part 4 - Defining callbacks that XHR object will call for us
-  xhr.onload = function(){
-  if (xhr.status === 200) {
-          reemplazarVariables(xhr.response, diccionario, listener)
-      }
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      reemplazarVariables(xhr.response, diccionario, listener)
+    }
   }
 
   xhr.onerror = function() {
-      console.log("Unable to load RSS")
+    console.log("Unable to load RSS")
   }
 
   // Part 5 - Starting the process
